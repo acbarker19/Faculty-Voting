3fe47c3484dca1c9d29775762cf87d5fb069416e
Faith Haiss <faithmarielle99@gmail.com>
2019-11-15 15:38:53 -0500

    Worked on add election button

--- EditElection.aspx.cs
+++ EditElection.aspx.cs
@@ -17,5 +17,31 @@ public partial class EditElection : System.Web.UI.Page
         string ElectionID = txtElectionID.Text;
         string StartDate = txtStartDate.Text;
         string EndDate = txtEndDate.Text;
+
+        if (ElectionID.Length > 0 && StartDate.Length > 0 && EndDate.Length > 0)
+        {
+            sdsElections.InsertParameters["ElectionID"].DefaultValue = ElectionID;
+            sdsElections.InsertParameters["StartDate"].DefaultValue = StartDate;
+            sdsElections.InsertParameters["EndDate"].DefaultValue = EndDate;
+
+            try
+            {
+                sdsElections.Insert();
+                lblStatus.Text = ElectionID + " : " + StartDate + " : " + EndDate + " : was added.";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error: Duplicate ElectionID. "
+                               + ElectionID + " : " + StartDate + " : " + EndDate + ": was not added.";
+            }
+        }
+        else
+        {
+            lblStatus.Text = "You must enter data into all the fields before pressing the add button.";
+        }
+
+        txtElectionID.Text = "";
+        txtStartDate.Text = "";
+        txtEndDate.Text = "";
     }
 }
